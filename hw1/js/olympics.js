dc57706c9f3863fdf3e282ad305ee6b37cf1e2e5
Michael Gallaspy <gallaspy.michael@gmail.com>
2020-01-15 21:59:06 -0800

    Add legend

--- hw1/js/olympics.js
+++ hw1/js/olympics.js
@@ -44,16 +44,17 @@ d3.tsv("data/olympics_series.tsv", function(data) {
       .call(d3.axisBottom(x));
     
     // Add Y axis
-    ymin = d3.min(data, function(d) { return d3.min(d.series, function(s) { return s.value }); });
-    ymax = d3.max(data, function(d) { return d3.max(d.series, function(s) { return s.value }); });
     var y = d3.scaleLinear()
-      .domain([ ymin, ymax ])
+      .domain([ 0, 1 ])
       .range([ height, 0 ]);
     svg.append("g")
       .call(d3.axisLeft(y));
     
+    // Add lines
+    var countries = [];
     data.forEach(function(d) {
         var country = d.country;
+        countries.push(country);
         var path = svg.append("path").datum(d.series)
           .attr("fill", "none")
           .attr("stroke", d3.schemeCategory10[d.color_index])
@@ -67,6 +68,45 @@ d3.tsv("data/olympics_series.tsv", function(data) {
           .style('mix-blend-mode', "multiply");
     });
     
+    // Add a legend
+    // Usually you have a color scale in your chart already
+    var color = d3.scaleOrdinal()
+      .domain(countries)
+      .range(d3.schemeCategory10);
+
+    var legendX = x.range()[1] * 0.8
+    var legendY = y(0.9);
+    var legend = svg.append("g")
+        .attr("id", "dot-container")
+        .attr("transform", `translate(${legendX},${legendY})`);
+    
+    // Add one dot in the legend for each name.
+    legend.selectAll("mydots")
+      .data(countries)
+      .enter()
+      .append("circle")
+        .attr("cx", 0)
+        .attr("cy", function(d,i){ return i*25})
+        .attr("r", 7)
+        .attr("class", "legend-dot")
+        .attr("id", function(d) { return d + "-dot"})
+        .style("fill", function(d){ return color(d)});
+
+    // Add one dot in the legend for each name.
+    legend.selectAll("mylabels")
+      .data(countries)
+      .enter()
+      .append("text")
+        .attr("x", 14)
+        .attr("y", function(d,i){ return i*25})
+        .style("fill", function(d){ return color(d)})
+        .text(function(d){ return d})
+        .attr("text-anchor", "left")
+        .attr("class", "legend-label")
+        .attr("id", function(d) { return d + "-label"})
+        .style("alignment-baseline", "middle");
+    
+    // Define what happens when the mouse moves.
     svg.style("position", "relative");
   
     d3.select("#viz svg").on("mouseenter", entered)
@@ -86,6 +126,7 @@ d3.tsv("data/olympics_series.tsv", function(data) {
 
     var date_extent = [];
     data[0].series.forEach(function (e) {date_extent.push(e.year)});
+
     function moved() {
         // When the mouse moves, find the closest point on one of the lines
         // Illustrate a dot there and highlight the line.
@@ -94,13 +135,12 @@ d3.tsv("data/olympics_series.tsv", function(data) {
         const xm = x.invert(d3.event.layerX - margin.left);
         const i1 = d3.bisectLeft(date_extent, xm);
         const i0 = Math.max(i1 - 1, 0);
-        console.log(i1, i0);
         const i = Math.abs(xm - date_extent[i0]) > Math.abs(xm - date_extent[i1]) ? i1: i0;
         const s = data.reduce(function(a, b){
             return Math.abs(a.series[i].value - ym) < Math.abs(b.series[i].value - ym) ? a : b
         });
         dot.attr("transform", `translate(${x(data[0].series[i].year)},${y(s.series[i].value)})`);
-        dot.select("text").text(s.country).style('background', 'white');
+        // dot.select("text").text(s.country).style('background', 'white');
         d3.selectAll(".seriesLine").attr("r", 10).style("stroke", "gray").style('mix-blend-mode', null)
             .attr("stroke-width", 1.5);
         d3.select("#" + s.country).attr("r", 10).style("stroke", d3.schemeCategory10[s.color_index]).attr("stroke-width", 2.0)
@@ -117,7 +157,6 @@ d3.tsv("data/olympics_series.tsv", function(data) {
             d3.select("#" + d.country).style("stroke", d3.schemeCategory10[d.color_index])
                 .style('mix-blend-mode', "multiply").attr("stroke-width", 1.5);
         });
-        //d3.selectAll(".seriesLine").attr("r", 10).style("stroke", "steelblue")
         dot.attr("display", "none");
     }
 });
